08fc45dc76dcb28009a09af403874d8185356d3c
AnastasiiaBilynska <nk015132@gmail.com>
2021-11-07 16:48:01 +0200

    codewars tasks have been completed

---
+++ Codewars tasks/7-kyu.rb
@@ -0,0 +1,35 @@
+=begin 
+Sum of the first nth term of Series
+
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+
+=end
+
+def series_sum(n)
+  result = 0.00
+  divider = 1
+  if n == 0 
+    return "0.00"
+  else
+    (1..n).each do |i|
+      result += 1.to_f / divider
+      divider += 3
+    end
+  end
+  "%.2f" % result.to_s
+end
